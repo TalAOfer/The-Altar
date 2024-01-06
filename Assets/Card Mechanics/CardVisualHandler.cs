e3d38c61acbb4c487babc2351d277502705b6be2
TalAOfer <talofer123@gmail.com>
2024-01-06 21:28:20 +0200

    New Card Material handling

--- Assets/Card Mechanics/CardVisualHandler.cs
+++ Assets/Card Mechanics/CardVisualHandler.cs
@@ -1,11 +1,16 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CardVisualHandler : MonoBehaviour
 {
     [SerializeField] private Card card;
     [SerializeField] private Animator anim;
+    [SerializeField] private Material shaderMaterial;
+    private Material cardMaterial;
+    private Material spritesMaterial;
 
     public SpriteFolder sprites;
 
@@ -15,10 +20,22 @@ public class CardVisualHandler : MonoBehaviour
     public SpriteRenderer numberSr;
     public SpriteRenderer symbolSr;
 
-    [SerializeField] private float fadeLerpTime;
+    [SerializeField] private float fadeLerpDuration;
+    [SerializeField] private ScriptableAnimationCurve fadeCurve;
 
     public void Init(CardBlueprint blueprint, string startingSortingLayer)
     {
+        cardMaterial = new Material(shaderMaterial);
+        cardSr.material = cardMaterial;
+        cardMaterial.SetColor("_Color", Color.white);
+        cardMaterial.SetColor("_Outline_Color", Color.black);
+        cardMaterial.SetInt("_Outline_On", 1);
+
+        spritesMaterial = new Material(shaderMaterial);
+        symbolSr.material = spritesMaterial;
+        iconSr.material = spritesMaterial;
+        numberSr.material = spritesMaterial;
+
         SetSpritesColor(blueprint.cardColor);
         symbolSr.sprite = GetSymbol();
         iconSr.sprite = blueprint.cardSprite;
@@ -54,9 +71,7 @@ public class CardVisualHandler : MonoBehaviour
     {
         card.cardColor = newColor;
         Color color = card.cardColor == CardColor.Black ? Color.black : Color.red;
-        numberSr.color = color;
-        symbolSr.color = color;
-        iconSr.color = color;
+        spritesMaterial.SetColor("_Color", color);
     }
 
     public void SetSortingOrder(int index)
@@ -97,23 +112,38 @@ public class CardVisualHandler : MonoBehaviour
         return sprite;
     }
 
-    public void OnReveal()
-    {
-        anim.Play("Card_Idle");
-        ShowSprites();
-    }
-
-    public void ShowSprites()
-    {
-        StartColorLerp(symbolSr, fadeLerpTime, false);
-        StartColorLerp(numberSr, fadeLerpTime, false);
-        StartColorLerp(iconSr, fadeLerpTime, false);
-    }
-
-
     public void StartColorLerp(SpriteRenderer spriteRenderer, float duration, bool toTransparent)
     {
         StartCoroutine(LerpColorCoroutine(spriteRenderer, duration, toTransparent));
+    }
+
+    [Button]
+    public void StartVanishLerp()
+    {
+        StartCoroutine(LerpVanish(cardMaterial, true));
+        StartCoroutine(LerpVanish(spritesMaterial, true)); 
+    }
+
+    private IEnumerator LerpVanish(Material material, bool toVisible)
+    {
+        float timeElapsed = 0f;
+        float startValue = toVisible ? -0.5f : 1f;
+        float endValue = toVisible ? 1f : -0.5f;
+
+        while (timeElapsed < fadeLerpDuration)
+        {
+            float t = timeElapsed / fadeLerpDuration;
+            float curveValue = fadeCurve.curve.Evaluate(t); // This will give you a value based on the curve's shape
+            // Now map the curveValue (0-1) to your desired range (-0.5 to 1)
+            float value = Mathf.Lerp(startValue, endValue, curveValue);
+            material.SetFloat("_Vanish", value);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Ensure the final value is set
+        material.SetFloat("_Vanish", endValue);
     }
 
     private IEnumerator LerpColorCoroutine(SpriteRenderer spriteRenderer, float duration, bool toTransparent)
---
+++ Assets/Shaders/Shaders/ScriptableAnimationCurve.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName ="AnimationCurve")]
+public class ScriptableAnimationCurve : ScriptableObject
+{
+    public AnimationCurve curve;
+}
